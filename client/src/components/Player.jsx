62390cf25d6acc108954f4b1882eeaea3bf7c8fc
Taras Yaroshko <taras.yaroshko@ucu.edu.ua>
2023-05-22 21:34:37 +0300

    Add base for music player

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -1,6 +1,28 @@
-import React from "react";
-import SpotifyPlayer from "react-spotify-web-playback";
+import { useState, useEffect } from "react"
+import SpotifyPlayer from "react-spotify-web-playback"
 
-const Player = ({ trackUri }) => {};
+export default function Player({ accessToken, trackUri }) {
+  const [play, setPlay] = useState(false)
 
-export default Player;
+  useEffect(() => setPlay(true), [trackUri])
+
+  if (!accessToken) return null
+  return (
+    <SpotifyPlayer
+      token={accessToken}
+      showSaveIcon
+      callback={state => {
+        if (!state.isPlaying) setPlay(false)
+      }}
+      play={play}
+      uris={trackUri ? [trackUri] : []}
+      styles={{ activeColor: '#fff',
+      bgColor: '#333',
+      color: '#fff',
+      loaderColor: '#fff',
+      sliderColor: '#1cb954', 
+      trackArtistColor: '#ccc', 
+      trackNameColor: '#fff' }}
+    />
+  )
+}
